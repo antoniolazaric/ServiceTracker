5e29a4aee38b2b47848ec7c19fab0db6ea7705a5
antoniolazaric <antoniolazaric1@gmail.com>
2026-06-25 22:11:23 +0200

    Spojen backend na supabase

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,16 @@
+require("dotenv").config();
 const express = require("express");
+const { createClient } = require("@supabase/supabase-js");
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_SECRET,
+);
 
 app.get("/api/health", (req, res) => {
   res.json({ ok: true, message: "Server radi" });
